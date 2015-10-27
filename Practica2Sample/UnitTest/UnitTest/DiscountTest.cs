e4a7790dc0e258b2c9c33f49c6c0c54c6283a1f3
MartinArispeRiveros <martinarisperiveros@gmail.com>
2015-10-27 18:01:39 -0400

    first commit

---
+++ Practica2Sample/UnitTest/UnitTest/DiscountTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+
+    [TestFixture]
+     class DiscountTest
+    {
+        [Test]
+        public void TestTransferFunds()
+        {
+            Account source = new Account();
+            source.Deposit(200.00F);
+            Account destination = new Account();
+            destination.Deposit(150.00F);
+
+            source.TransferFunds(destination, 100.00F);
+            Assert.AreEqual(250.00F, destination.Balance);
+            Assert.AreEqual(100.00F, source.Balance);
+
+        }
+    }
+}

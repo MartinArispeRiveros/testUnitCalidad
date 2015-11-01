2fc2be65d3cc129c32494d0ce8b8abf84c141e35
sergioarispejulio <sergio_arispe@hotmail.com>
2015-11-01 19:29:28 -0400

    Algunos tests para Account

--- Practica2Sample/UnitTest/UnitTest/AccountTest.cs
+++ Practica2Sample/UnitTest/UnitTest/AccountTest.cs
@@ -9,18 +9,42 @@ namespace UnitTest
   [TestFixture]
   public class AccountTest
   {
-    [Test]
-    public void TestTransferFunds()
-    {
-      Account source = new Account();
-      source.Deposit(200.00F);
-      Account destination = new Account();
-      destination.Deposit(150.00F);
 
-      source.TransferFunds(destination, 100.00F);
-      Assert.AreEqual(250.00F, destination.Balance);
-      Assert.AreEqual(100.00F, source.Balance);
+      [Test]
+      public void TestTransferFunds()
+      {
+        Account source = new Account();
+        source.Deposit(200.00F);
+        Account destination = new Account();
+        destination.Deposit(150.00F);
+
+        source.TransferFunds(destination, 100.00F);
+        Assert.AreEqual(250.00F, destination.Balance);
+        Assert.AreEqual(100.00F, source.Balance);
 	
-    }
+      }
+
+      [Test]
+      public void TestDeposit()
+      {
+          Account source = new Account();
+          source.Deposit(200.00F);
+
+          Assert.AreEqual(200.00F, source.Balance);
+
+      }
+
+      public void TestWithdraw()
+      {
+          Account source = new Account();
+          source.Withdraw(2.00F);
+
+          Assert.AreEqual(-2.00F, source.Balance);
+
+      }
+    
+
+
+
   }
 }
